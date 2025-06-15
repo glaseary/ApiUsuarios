c47fb241e1f057d93e01dd76952e9bd8674c41b4
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 23:46:33 -0400

    primer commit

---
+++ src/main/java/com/Perfulandia/ApiUsuarios/ApiUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandia/ApiUsuarios/ApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
